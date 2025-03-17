71369a5d89cb5bc103b6dabddbdc7cc5999e38da
Phi Blackhole <nguyenminhphi25042005@gmail.com>
2025-03-17 20:24:50 +0700

    Change main.jsx structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route}  from 'react-router-dom'
+import MainPage from './routes/MainPage.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+    <Routes>
+      <Route path ="/" element={<App/>}></Route>
+      <Route path="main" element={<MainPage/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )

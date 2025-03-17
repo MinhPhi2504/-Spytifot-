170b94c93ae9a7064b45d93d4bdf380170f449c0
Phi Blackhole <nguyenminhphi25042005@gmail.com>
2025-03-17 16:58:03 +0700

    Add MainPage

---
+++ src/routes/MainPage.jsx
@@ -0,0 +1,11 @@
+function MainPage() {
+    return (
+      <div className="container mt-5 text-white">
+        <h1 className="text-center">Welcome to the Main Page!</h1>
+        <p className="text-center">Bạn đã đăng ký thành công.</p>
+      </div>
+    );
+  }
+  
+  export default MainPage;
+  

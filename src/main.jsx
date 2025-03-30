93c9c94decdb641c489a57f3c15e90812ef395d7
Phi Blackhole <nguyenminhphi25042005@gmail.com>
2025-03-30 17:44:49 +0700

    change route to MainPage

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
     <Routes>
       <Route path ="/" element={<App/>}></Route>
-      <Route path="main" element={<MainPage/>}></Route>
+      <Route path="/main" element={<MainPage/>}></Route>
     </Routes>
     </BrowserRouter>
   </StrictMode>

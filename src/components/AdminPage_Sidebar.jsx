5b84f6861b9d89e08234cfaa3b97a94bed2878e0
Phi Blackhole <nguyenminhphi25042005@gmail.com>
2025-05-30 21:14:56 +0700

    OK

--- src/components/AdminPage_Sidebar.jsx
+++ src/components/AdminPage_Sidebar.jsx
@@ -7,21 +7,21 @@ function AdminPage_Sidebar() {
     <div className="sidebar2">
       <ul className="sidebar-menu">
         <li>
-           <Link to="/admin/dashboard" className="sidebar-link">
+           <Link to="/admin/dashboard" className=" element sidebar-link">
               <FaTachometerAlt />
               <span>Dashboard</span>
             </Link>
         </li>
         <li>
-           <Link to="/admin/QLBH" className="QLBH">
+           <Link to="/admin/QLBH" className=" element QLBH">
               <FaTachometerAlt />
               <span>Quản lý bài hát</span>
             </Link>
         </li>
-        <li><FaUser /> <span>Quản lý nghệ sĩ</span></li>
-        <li><FaCompactDisc /> <span>Quản lý album/playlist</span></li>
-        <li><FaUsers /> <span>Quản lý người dùng</span></li>
-        <li><FaChartBar /> <span>Thống kê và báo cáo</span></li>
+        <li className="element"><FaUser /> <span>Quản lý nghệ sĩ</span></li>
+        <li className="element"><FaCompactDisc /> <span>Quản lý album/playlist</span></li>
+        <li className="element"><FaUsers /> <span>Quản lý người dùng</span></li>
+        <li className="element"><FaChartBar /> <span>Thống kê và báo cáo</span></li>
       </ul>
     </div>
   );
